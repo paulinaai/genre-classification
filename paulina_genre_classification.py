c03d75270c5c630db7d7b09f09c90576f3a35ecb
Paulina Ai <paulinaa2407@gmail.com>
2024-05-06 11:42:26 -0700

    yay:

--- paulina_genre_classification.py
+++ paulina_genre_classification.py
@@ -9,11 +9,27 @@ from tensorflow.keras import Sequential
 
 VALIDATION_SPLIT = 0.3
 
-df = pd.read_csv('features_30_sec.csv')
-df.pop('filename')
-label = df.pop('label')
+df_train = pd.read_csv('features_30_sec.csv', names = ["filename", "length",
+        "chroma_stft_mean", "chroma_stft_var", "rms_mean",	"rms_var", "spectral_centroid_mean", 
+         "spectral_centroid_var", "spectral_bandwidth_mean", "spectral_bandwidth_var", "rolloff_mean", 
+         "rolloff_var", "zero_crossing_rate_mean", "zero_crossing_rate_var", "harmony_mean", "harmony_var", 
+         "perceptr_mean", "perceptr_var", "tempo", "mfcc1_mean", "mfcc1_var", "mfcc2_mean", "mfcc2_var", 
+         "mfcc3_mean", "mfcc3_var", "mfcc4_mean", "mfcc4_var", "mfcc5_mean", "mfcc5_var", "mfcc6_mean", 
+         "mfcc6_var", "mfcc7_mean", "mfcc7_var", "mfcc8_mean", "mfcc8_var", "mfcc9_mean", "mfcc9_var", 
+         "mfcc10_mean", "mfcc10_var", "mfcc11_mean", "mfcc11_var", "mfcc12_mean", "mfcc12_var", "mfcc13_mean", 
+         "mfcc13_var", "mfcc14_mean", "mfcc14_var", "mfcc15_mean", "mfcc15_var", "mfcc16_mean", "mfcc16_var", 
+         "mfcc17_mean", "mfcc17_var", "mfcc18_mean", "mfcc18_var", "mfcc19_mean", "mfcc19_var", "mfcc20_mean", 
+         "mfcc20_var", "label"
+         ])
+df_train.pop("filename")
+df_train.pop("length")
+df_train = df_train.iloc[1:]
+df_features = df_train.copy()
+df_labels = df_features.pop('label')
+print(df_train.head()) # just for funsies
 
-df.head()
+df_features = np.array(df_features)
+print(df_features)
 
 # names = [
 #         "chroma_stft_mean", "chroma_stft_var", "rms_mean",	"rms_var", "spectral_centroid_mean", 
@@ -35,13 +51,13 @@ df.head()
 #     newTuple = (df1, df2)
 #     return newTuple
 
-tupleList = []
-for index, row in df.iterrows():
-    print(row)
-    if index == 10: 
-        quit()
-    # newTuple = 
-    # tupleList += newTuple
+# tupleList = []
+# for index, row in df.iterrows():
+#     print(row)
+#     if index == 10: 
+#         quit()
+#     # newTuple = 
+#     # tupleList += newTuple
 
     
 
